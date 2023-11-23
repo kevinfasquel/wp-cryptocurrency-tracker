19bd0562f07e94bdea653e41ffc9eeefaf6ab434
Kévin Fasquel <kevin.fasquel@proton.me>
2023-11-23 20:16:58 +0100

    Initial Commit

---
+++ cryptocurrency-tracker.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Cryptocurrency Tracker
+ * Plugin URI:        
+ * Description:       Simple Cryptocurrency tracker. With multiple Widget and Pop-up.
+ * Version:           1.0.0
+ * Author:            Kévin Fasquel
+ * Author URI:        
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       cryptocurrency-tracker
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+    die;
+}
+
+define('PLUGIN_NAME_VERSION', '1.0.0');
